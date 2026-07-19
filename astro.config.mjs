654d534f6c1f23205e4e0fc8c54b188c4dcbefdf
Pratyay360 <pratyaymustafi@outlook.com>
2026-07-19 18:58:24 +0530

    update

---
+++ astro.config.mjs
@@ -0,0 +1,81 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightOpenAPI, { openAPISidebarGroups } from "starlight-openapi";
+// import min from "astro-min";
+import playformCompress from "@playform/compress";
+// import { classShortener } from "astro-class-shortener";
+// import starlightLinksValidator from "starlight-links-validator";
+// https://astro.build/config
+export default defineConfig({
+	compressHTML: true,
+	integrations: [
+		(await import("@playform/compress")).default({
+			CSS: true,
+			HTML: true,
+			Image: false,
+			JavaScript: true,
+			JSON: true,
+			SVG: true,
+		}),
+		starlight({
+			title: "forgit api docs",
+			 plugins: [
+				starlightOpenAPI([
+					{
+						base: "github",
+						schema:
+							"https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json",
+							snippets: {
+								operation: false,
+							},
+					},
+					{
+						base: "gitlab",
+						schema:
+							"https://gitlab.com/gitlab-org/gitlab/-/raw/master/doc/api/openapi/openapi_v3.yaml?ref_type=heads",
+							snippets: {
+								operation: false,
+							},
+
+					},
+					{
+						base: "bitbucket",
+						schema:
+							"https://dac-static.atlassian.com/cloud/bitbucket/swagger.v3.json",
+							snippets: {
+								operation: false,
+							},
+					},
+					{
+						base: "forgejo",
+						// schema: "https://v15.next.forgejo.org/swagger.v1.json",
+						schema: "./src/assets/codeberg.json",
+						snippets: {
+							operation: false,
+						},
+					},
+				]),
+			],
+			sidebar: [
+				{
+					label: "forgit app",
+					items: [
+						{
+							label: "Sourcehut",
+							items: [{ autogenerate: { directory: "sourcehut" } }],
+						},
+						...openAPISidebarGroups,
+					],
+				},
+
+				{
+					label: "Reference",
+					items: [{ autogenerate: { directory: "reference" } }],
+				},
+			],
+		}),
+
+		playformCompress(),
+	],
+});
---
+++ graphql.config.ts
@@ -0,0 +1,15 @@
+// export default {
+// 	schema: fetch(
+// 		"https://git.sr.ht/~sircmpwn/git.sr.ht/blob/master/api/graph/schema.graphqls",
+// 	),
+// 	extensions: {
+// 		"graphql-markdown": {
+// 			rootPath: "./src/content/docs",
+// 			baseURL: "/sourcehut",
+// 			printTypeOptions: {
+// 				typeBadges: true,
+// 			},
+// 			formatter: "@graphql-markdown/formatters/starlight"
+// 		},
+// 	},
+// };

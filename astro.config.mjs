7ebe45a33149c4da5a6e5dc109553513ef7cec1c
Pratyay360 <pratyaymustafi@outlook.com>
2026-07-23 15:12:00 +0530

    fixup! baseimage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,54 +3,52 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightOpenAPI, { openAPISidebarGroups } from "starlight-openapi";
 // import min from "astro-min";
-import playformCompress from "@playform/compress";
+// import playformCompress from "@playform/compress";
 // import { classShortener } from "astro-class-shortener";
 // import starlightLinksValidator from "starlight-links-validator";
 // https://astro.build/config
 export default defineConfig({
-	compressHTML: true,
-	integrations: [
-		playformCompress({
-			CSS: true,
-			HTML: false,
-			Image: false,
-			JavaScript: false,
-			JSON: true,
-			SVG: true,
-		}),
+  integrations: [
 		starlight({
 			title: "forgit api docs",
-			 plugins: [
+			plugins: [
 				starlightOpenAPI([
 					{
 						base: "github",
 						schema:
 							"https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json",
-							snippets: {
-								operation: false,
-							},
+						snippets: {
+							operation: false,
+						},
 					},
 					{
 						base: "gitlab",
 						schema:
 							"https://gitlab.com/gitlab-org/gitlab/-/raw/master/doc/api/openapi/openapi_v3.yaml?ref_type=heads",
-							snippets: {
-								operation: false,
-							},
+						snippets: {
+							operation: false,
+						},
 
 					},
 					{
 						base: "bitbucket",
 						schema:
 							"https://dac-static.atlassian.com/cloud/bitbucket/swagger.v3.json",
-							snippets: {
-								operation: false,
-							},
+						snippets: {
+							operation: false,
+						},
 					},
 					{
 						base: "forgejo",
+						// schema: "",
+						schema: "https://v15.next.forgejo.org/swagger.v1.json",
+						snippets: {
+							operation: false,
+						},
+					},{
+						base: "codeberg",
 						// schema: "https://v15.next.forgejo.org/swagger.v1.json",
-						schema: "./src/assets/codeberg.json",
+						schema: "https://v15.next.forgejo.org/swagger.v1.json",
 						snippets: {
 							operation: false,
 						},
--- graphql.config.ts
+++ graphql.config.ts
@@ -1,15 +1,13 @@
-// export default {
-// 	schema: fetch(
-// 		"https://git.sr.ht/~sircmpwn/git.sr.ht/blob/master/api/graph/schema.graphqls",
-// 	),
-// 	extensions: {
-// 		"graphql-markdown": {
-// 			rootPath: "./src/content/docs",
-// 			baseURL: "/sourcehut",
-// 			printTypeOptions: {
-// 				typeBadges: true,
-// 			},
-// 			formatter: "@graphql-markdown/formatters/starlight"
-// 		},
-// 	},
-// };
+export default {
+	schema: "https://git.sr.ht/~sircmpwn/git.sr.ht/blob/master/api/graph/schema.graphqls",
+	extensions: {
+		"graphql-markdown": {
+			rootPath: "./src/content/docs",
+			baseURL: "sourcehut",
+			formatter: "@graphql-markdown/formatters/starlight",
+			loaders: {
+				UrlLoader: "@graphql-tools/url-loader",
+			},
+		},
+	},
+};
